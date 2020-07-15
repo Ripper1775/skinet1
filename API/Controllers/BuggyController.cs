12eb15b71c5b2a2ca5893604f453822e02b9b7b2
ripper1775 <ripduplain@comcast.net>
2020-07-15 10:17:32 -0500

    end of Section 11

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -16,7 +16,12 @@ namespace API.Controllers
         public ActionResult GetNotFoundRequest()
         {
             var thing = _context.Products.Find(42);
-            if (thing == null) { return NotFound(new ApiResponse(404)); }
+
+            if (thing == null) 
+            {
+                return NotFound(new ApiResponse(404));
+            }
+
             return Ok();
         }
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { HomeModule } from './home/home.module';
 import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ShopModule } from './shop/shop.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -21,7 +21,9 @@ import { ShopModule } from './shop/shop.module';
     CoreModule,
     HomeModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
